1e24e36e462a71560591729bb3f31a79d4a5ccd3
Jaco Erasmus <jierasmus27@gmail.com>
2017-02-12 21:36:21 +0200

    Add route tests for comparison controller

---
+++ spec/routes/comparison_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "Routes", :type => "routing" do
+  describe "Comparison" do
+    let(:primary_id) { "1" }
+    let(:secondary_id) { "2" }
+
+    it "routes to #list for the default route" do
+      expect(get("/")).to route_to(:controller => "comparison", :action => "list")
+    end
+
+    it "routes to #compare for form submissions" do
+      expect(post("/compare")).to route_to(:controller => "comparison", :action => "compare")
+    end
+  end
+end
