a648e9adafc0409327f7a58148eed752d450b0e9
Jaco Erasmus <jierasmus27@gmail.com>
2017-02-17 16:13:35 +0200

    Clean up comparison controller tests

---
+++ spec/factories/starship.rb
@@ -0,0 +1,28 @@
+FactoryGirl.define do
+  # factory :starship, class: Starship do
+  #   skip_create
+  #
+  #   id { rand(100) }
+  #   name { "Snowspeeder" }
+  #   model { "t-47 airspeeder" }
+  #   manufacturer { "Incom corporation" }
+  #   cost_in_credits { "unknown" }
+  #   length { "4.5" }
+  #   max_atmosphering_speed { "650" }
+  #   crew { "2" }
+  #   passenger { "0" }
+  #   cargo_capacity { "10" }
+  #   consumables { "none" }
+  #   vehicle_class { "airspeeder" }
+  #   pilots { [
+  #     "http://swapi.co/api/people/1/",
+  #     "http://swapi.co/api/people/18/"
+  #   ] }
+  #   films { [
+  #     "http://swapi.co/api/films/2/"
+  #   ] }
+  #   created { "2014-12-15T12:22:12Z" }
+  #   edited { "2014-12-22T18:21:15.623033Z" }
+  #   url { "http://swapi.co/api/vehicles/14/" }
+  # end
+end
