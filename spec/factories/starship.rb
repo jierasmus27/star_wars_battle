92e26997ed7611efbddc3f8d59f7cfea898c4af5
Jaco Erasmus <jierasmus27@gmail.com>
2017-02-17 16:28:26 +0200

    Refactor person spec by using factories

--- spec/factories/starship.rb
+++ spec/factories/starship.rb
@@ -1,28 +1,40 @@
 FactoryGirl.define do
-  # factory :starship, class: Starship do
-  #   skip_create
-  #
-  #   id { rand(100) }
-  #   name { "Snowspeeder" }
-  #   model { "t-47 airspeeder" }
-  #   manufacturer { "Incom corporation" }
-  #   cost_in_credits { "unknown" }
-  #   length { "4.5" }
-  #   max_atmosphering_speed { "650" }
-  #   crew { "2" }
-  #   passenger { "0" }
-  #   cargo_capacity { "10" }
-  #   consumables { "none" }
-  #   vehicle_class { "airspeeder" }
-  #   pilots { [
-  #     "http://swapi.co/api/people/1/",
-  #     "http://swapi.co/api/people/18/"
-  #   ] }
-  #   films { [
-  #     "http://swapi.co/api/films/2/"
-  #   ] }
-  #   created { "2014-12-15T12:22:12Z" }
-  #   edited { "2014-12-22T18:21:15.623033Z" }
-  #   url { "http://swapi.co/api/vehicles/14/" }
-  # end
+  factory :starship, class: Starship do
+    skip_create
+
+    model { "Sentinel-class landing craft" }
+    manufacturer { "Sienar Fleet Systems, Cyngus Spaceworks" }
+    cost_in_credits { "240000" }
+    consumables { "1 month" }
+    mglt { "70" }
+    starship_class { "landing craft" }
+    pilots { [] }
+    films { [
+      "http://swapi.co/api/films/1/"
+    ] }
+    created { "2014-12-10T15:48:00.586000Z" }
+    edited { "2014-12-22T17:35:44.431407Z" }
+
+    trait :sentinel do
+      name { "Sentinel-class landing craft" }
+      hyperdrive_rating { "1.0" }
+      length { "38" }
+      max_atmosphering_speed { "1000" }
+      crew { "5" }
+      passengers { "75" }
+      cargo_capacity { "180000" }
+      url { "http://swapi.co/api/starships/5/" }
+    end
+
+    trait :death_star do
+      name { "Death Star" }
+      hyperdrive_rating { "4.0" }
+      length { "120000" }
+      max_atmosphering_speed { "1" }
+      crew { "342953" }
+      passengers { "843342" }
+      cargo_capacity { "1000000000000" }
+      url { "http://swapi.co/api/starships/9/" }
+    end
+  end
 end
